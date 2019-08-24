608c67ecf898d16b9ed378a77ac4b86ebead1673
BRI0004 <live.com.aulive.com.au@live.com.au>
2019-08-24 18:41:52 +1000

    aaaaaaa

--- GameNode/server.js
+++ GameNode/server.js
@@ -34,26 +34,38 @@ cloak.configure({
       user.message('listRooms', cloak.getRooms(true));
       console.log("List Rooms " + cloak.getRooms(true));
     },
-
+    //////////////////////////////////////////////
     listUsers: function(arg, user) {
-      user.message('refreshLobby', {
+      user.message('refreshRoom', {
         users: user.room.getMembers(true),
-        inLobby: user.room.isLobby,
-        roomCount: user.room.getMembers().length,
-        roomSize: user.room.size
+        roomCount: user.room.getMembers().length
       });
     },
     createRoom: function(arg, user) {
-      var room = cloak.createRoom(Math.floor(Math.random()*100000-1), 1000);
+      var room = cloak.createRoom(Math.floor(Math.random()*100000-1), 100);
+      user.name = arg
       var success = room.addMember(user);
       user.message('roomCreated', {
         success: success,
         roomId: room.id,
-        roomName: room.name,
-        user: user,
-        room: room
+        roomName: room.name
       });
-
+    },
+    joinRoomFromName: function(arg,user)  {
+      var rooms = cloak.getRooms();
+      console.log(" ROOOMS " + rooms);
+      console.log(arg.x);
+      for (index = 0; index < rooms.length; ++index) {
+        console.log(" current index "  + rooms[index][1]);
+        if (rooms[index][1] == arg[0]) {
+          console.log("FOUND");
+          user.name = arg[1];
+          rooms[index].addMember(user);
+          break;
+        } else {
+          console.log('aihlsdfgaydsgfhadgfsdjf');
+        }
+      }
     }
 
   },
@@ -72,8 +84,7 @@ cloak.configure({
 
 
       */
-
-
+      console.log(cloak.getRooms(true));
       console.log("Room Initation " + this.name + " " + this.id);
     },
 
