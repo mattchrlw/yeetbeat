4d407ff13b1e6a051c2d377b4b470586badb29f1
BRI0004 <live.com.aulive.com.au@live.com.au>
2019-08-24 13:38:19 +1000

    yeet 2

---
+++ GameNode/server.js
@@ -0,0 +1,34 @@
+/* jshint node:true */
+
+var cloak = require('cloak');
+var _ = require('underscore');
+var connect = require('connect');
+
+var clientPort = 8080;
+var serverPort = 8090;
+
+var sendLobbyCount = function(arg) {
+  this.messageMembers('userCount', this.getMembers().length);
+  this.messageMembers('chat', "succ my weewee");
+};
+
+cloak.configure({
+  port: serverPort,
+  messages: {
+    chat: function(msg, user) {
+      user.getRoom().messageMembers('chat', msg);
+    }
+  },
+  lobby: {
+    newMember: sendLobbyCount,
+    memberLeaves: sendLobbyCount,
+  }
+});
+
+cloak.run();
+
+connect()
+.use(connect.static('./client'))
+.listen(clientPort);
+
+console.log('client running on on ' + clientPort);
