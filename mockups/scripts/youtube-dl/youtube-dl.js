1b17bd3ec3ae7756a2d9d3aaff8105a72e233c3d
Anwealso <alnicholo@gmail.com>
2019-08-24 14:23:00 +1000

    Addded some youtube-dl

---
+++ mockups/scripts/youtube-dl/youtube-dl.js
@@ -0,0 +1,9 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    // We will put code here later
+}
