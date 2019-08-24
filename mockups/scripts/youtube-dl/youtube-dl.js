7c933440e31449ce72f085e2373bdc3a9965c28d
Anwealso <alnicholo@gmail.com>
2019-08-24 14:23:09 +1000

    .

--- mockups/scripts/youtube-dl/youtube-dl.js
+++ mockups/scripts/youtube-dl/youtube-dl.js
@@ -1,9 +1,14 @@
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
+
 convertBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
     sendURL(URLinput.value);
 });
+
 function sendURL(URL) {
-    // We will put code here later
+    fetch(`http://localhost:4000/download?URL=${URL}`, {
+        method:'GET'
+    }).then(res => res.json())
+    .then(json => console.log(json));
 }
